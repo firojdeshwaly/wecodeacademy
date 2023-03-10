416cf072da8eb7a803946eff32eb844d0d33a42b
firoj khan <firoj.deshwaly@gmail.com>
2023-03-10 22:35:26 +0530

    array test

---
+++ test2.js
@@ -0,0 +1,119 @@
+// //Q1:
+// let array = [1,2,3];
+// console.log(array[1]);
+
+// //Q2:
+// let array2 = [1,2,3,4,5];
+// console.log(array2.length);
+
+// //Q3:
+// let array3 = [1,2,3,4];
+// for(let i=0; i<array3.length; i++){
+//     console.log(array3[i]);
+// }
+
+//Q4:
+// let array4 = [1,2,3,4,5,6];
+// array3.forEach((i) => {
+//     console.log(i);
+// })
+
+//Q5:
+// let array5 = [1,2,3];
+// array5.push(4);
+// console.log(array5)
+
+//Q6
+// let array6 = [1,2,3,4];
+// array6.splice(1,1)
+// console.log(array6)
+
+//Q7
+// let array7 = [1,2,3,4,5];
+// array7.pop();
+// console.log(array7)
+
+//Q8
+// let array8 = [1,2,3];
+// if(array8.includes(3)){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//Q9:
+// let array9 = [1,12,3,4];
+// array9.sort(ascending);
+// console.log(array9);
+// function ascending(a,b){
+//    return a-b;
+// }
+
+//Q10:
+// let array10 = [1,12,3,4];
+// array9.sort(descending);
+// console.log(array9);
+// function descending(a,b){
+//    return b-a;
+// }
+
+
+//Q11
+// let Array1 = [1,12,3,4];
+// let Array2 = [11,22,32,44];
+// let concatenateArray = Array1.concat(Array2);
+// console.log(concatenateArray);
+
+//Q12
+// let array = [1,12,3];
+// let stringArray = array.map(num => num.toString());
+// console.log(stringArray); 
+
+//Q13
+// let array = [1,12,3,4];
+// array.reverse();
+// console.log(array);
+
+//Q14
+// let array = ["apple", "banana", "cherry", "date", "elderberry"];
+// let index = array.indexOf("banana");
+// console.log(index);
+
+//Q15
+// let array = [1,2,3,4,5,6];
+// let sliceArray = array.slice(0,3);
+// console.log(sliceArray);
+
+//Q16
+// let array = [1, 2, 3, 4, 5, 6];
+// let doubledArray = array.map(num => num * 2);
+// console.log(doubledArray);
+
+//Q17
+// let array = [10, 20, 30, 40];
+// let i = 0;
+// let sum = 0;
+// while(i<array.length){
+//     sum = sum + array[i];
+//     i++
+// }
+// console.log(sum);
+
+//Q18
+// let array = [10, 25, 31, 40];
+// let evenNumber = array.filter(even);
+// function even(number){
+//     return number % 2 ==0;
+// }
+// console.log(evenNumber);
+
+//Q19
+// let array = ["date",10, 03, 2023,"toDay"];
+// let joinConcatenate = array.join("-");
+// console.log(joinConcatenate);
+
+//Q20
+// let array1 = ["apple", "banana", "cherry"];
+// let array2 = ["date", "elderberry", "fig"];
+// let newArray = array1.concat(array2);
+// console.log(newArray);
